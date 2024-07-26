3213c3485d23fd11bb52f640347a4dd0059e4064
amsohag007 <amsohag007@gmail.com>
2024-07-27 02:27:11 +0600

    signup api function

---
+++ api/signup.js
@@ -0,0 +1,24 @@
+const bcrypt = require('bcrypt');
+const { Client } = require('pg');
+
+module.exports = async (req, res) => {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: 'Username and password are required' });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const client = new Client();
+  await client.connect();
+
+  const query = 'INSERT INTO users (username, password) VALUES ($1, $2)';
+  await client.query(query, [username, hashedPassword]);
+
+  client.end();
+  res.status(201).json({ message: 'User created' });
+};
