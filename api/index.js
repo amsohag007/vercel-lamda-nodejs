86e7f7e160786a3b3894bff86fbb1328d2b90fd8
amsohag007 <amsohag007@gmail.com>
2024-07-30 02:47:37 +0600

    swagger-setup-update

--- api/index.js
+++ api/index.js
@@ -1,12 +1,23 @@
-// module.exports = (req, res) => {
-//   if (req.method === 'GET') {
-//     res.status(200).send('Welcome to Income Path Vercel Serverless API Functions');
-//   } else {
-//     res.status(405).send('Method Not Allowed');
-//   }
-// };
+const express = require('express');
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger');
 
-const serverless = require('serverless-http');
-const app = require('../index');
+const app = express();
 
-module.exports.handler = serverless(app);
+app.use(express.json());
+
+// Set up Swagger
+swaggerDocument(app);
+
+// Dummy root route
+app.get('/', (req, res) => {
+  res.send('Welcome to Vercel Serverless API Functions');
+});
+
+// API routes
+app.use('/api/signup', require('./signup'));
+app.use('/api/request-reset-password', require('./request-reset-password'));
+app.use('/api/reset-password', require('./reset-password'));
+
+module.exports = app; // Ensure that app is exported correctly
