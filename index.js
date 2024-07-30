8403591bf555f7c24ad4fd6ed4c670c9140c2088
perassi <horacio@incomepath.com>
2024-07-30 13:32:35 +0600

    swagger test1

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const serverless = require('serverless-http');
+const swaggerDocument = require('./swagger');
+
+const app = express();
+app.use(express.json());
+
+// Set up Swagger
+app.use('/api-docs', swaggerDocument);
+
+// Dummy root route
+app.get('/', (req, res) => {
+  res.send('Welcome to Vercel Serverless API Functions');
+});
+
+// API routes
+app.use('/api/signup', require('./api/signup'));
+app.use('/api/request-reset-password', require('./api/request-reset-password'));
+// Add more routes as needed
+
+module.exports = app; // Ensure that app is exported correctly
