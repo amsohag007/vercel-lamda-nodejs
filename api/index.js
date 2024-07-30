c862db7d391446876638c32d2369197d9b8a8812
perassi <horacio@incomepath.com>
2024-07-30 14:07:40 +0600

    swagger test6

--- api/index.js
+++ api/index.js
@@ -1,14 +1,5 @@
-import express from "express";
-import app from "../app"; // Adjust the path as necessary
+import app from '../app'; // Adjust the path as necessary
 
-const server = express();
-
-server.use((req, res) => {
-  if (!req.url.startsWith("/api-docs")) {
-    return res.status(404).send("Not Found");
-  }
-
+export default async function handler(req, res) {
   app(req, res);
-});
-
-export default server;
+}
--- api/swagger.js
+++ api/swagger.js
@@ -3,8 +3,7 @@ import swaggerJsDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import express from "express";
 
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
 const swaggerOptions = {
   swaggerDefinition: {
@@ -20,15 +19,14 @@ const swaggerOptions = {
       },
     ],
   },
-  apis: ["./*.js"],
+  apis: ["./*.js"], // Adjust the path as necessary
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 const router = express.Router();
 
 router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocs, {
-  customCss:
-    '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+  customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
   customCssUrl: CSS_URL,
 }));
 
