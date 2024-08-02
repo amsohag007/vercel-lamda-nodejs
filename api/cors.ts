dfc2a016d002f3890b301f5ba56f6d84dddc327b
amsohag007 <amsohag007@gmail.com>
2024-08-03 01:40:30 +0600

    authorization header added to cors

--- api/cors.ts
+++ api/cors.ts
@@ -1,4 +1,3 @@
-// api/cors.ts
 const allowCors = (fn: (req: any, res: any) => Promise<void>) => async (req: any, res: any) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -7,7 +6,7 @@ const allowCors = (fn: (req: any, res: any) => Promise<void>) => async (req: any
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    'Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
   if (req.method === 'OPTIONS') {
     res.status(200).end();
